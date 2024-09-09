e748aed590ccbee2060424b8ed6bf37cd0160ffd
Matheus Felipe Braga <matheusfbraga10@gmail.com>
2024-09-09 20:19:43 -0300

    Instalação correta e inicio dos estudos
    09/09/24

---
+++ ProjetoExtencao/App.js
@@ -0,0 +1,22 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Projeto de Extenção.</Text>
+      <Text>Juliano, Marilza e Matheus.</Text>
+      <Text>Aplicativo Android com React</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
